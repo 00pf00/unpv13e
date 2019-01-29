3953fa17967ffed9f10adcdedfa941a754b7578a
pf <408445889@qq.com>
2019-01-29 20:45:35 +0800

    工程文件提交

---
+++ cmake-build-debug/CMakeFiles/unpv13e.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unpv13e.dir/intro/daytimetcpcli.c.o"
+  "CMakeFiles/unpv13e.dir/lib/error.c.o"
+  "unpv13e.pdb"
+  "unpv13e"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unpv13e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
