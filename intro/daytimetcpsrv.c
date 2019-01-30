5e8cdaa18799c425e73a1be3066da37a84ad8de7
00pf00 <408445889@qq.com>
2019-01-30 11:18:58 +0800

    server行数对齐

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -1,6 +1,5 @@
 #include	"unp.h"
 #include	<time.h>
-
 int
 main(int argc, char **argv)
 {
@@ -8,25 +7,18 @@ main(int argc, char **argv)
 	struct sockaddr_in	servaddr;
 	char				buff[MAXLINE];
 	time_t				ticks;
-
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
-
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port        = htons(13);	/* daytime server */
-
 	Bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
-
 	Listen(listenfd, LISTENQ);
-
 	for ( ; ; ) {
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
-
         ticks = time(NULL);
         snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks)); //int snprintf(char* dest_str,size_t size,const char* format,...)
         Write(connfd, buff, strlen(buff));                        //先将可变参数 “…” 按照format的格式格式化为字符串，然后再将其拷贝至dest_str中
-
 		Close(connfd);
 	}
 }
