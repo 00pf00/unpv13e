dc39a697f93cf09b6edc1957995597865c00d6b9
00pf00 <408445889@qq.com>
2019-01-30 09:09:01 +0800

    添加snprintf函数注释

--- intro/daytimetcpsrv.c
+++ intro/daytimetcpsrv.c
@@ -24,8 +24,8 @@ main(int argc, char **argv)
 		connfd = Accept(listenfd, (SA *) NULL, NULL);
 
         ticks = time(NULL);
-        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        Write(connfd, buff, strlen(buff));
+        snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks)); //int snprintf(char* dest_str,size_t size,const char* format,...)
+        Write(connfd, buff, strlen(buff));                        //先将可变参数 “…” 按照format的格式格式化为字符串，然后再将其拷贝至dest_str中
 
 		Close(connfd);
 	}
